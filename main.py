76d7701244a5e55ecf49c645a50ffa3f34d7c18c
tkrumpol <tkrumpol@andrew.cmu.edu>
2022-05-23 23:34:28 -0400

    started working on second excel sheet for median rent by bedrooms. Updated calls to new functions in utils.py

--- main.py
+++ main.py
@@ -7,54 +7,57 @@ Created April 2022
 
 main function to examine bp rental data
 
-pandas 1.3.5
+pandas 1.3.5 for reading .xlsx files. 
 
 
 Notes:
     - Cities are in all caps
     - Mapper for index to city might be able to be reused for each df
+       -- If turned into class then make this common attribute
 
 Idea
 - given an input city, return dataframe with information from each sheet
+
+Sheet Names
+- 'Median Rent & Number of Listing' 
+- 'Median Rent_YoY (%)'
+- 'Median Rent by Bedrooms'
+- 'Median Rent by Bedrooms YoY(%)' 
+- 'Median Rent by Type'
+- 'Median Rent by Type YoY(%)'
+
+[ ] df = data['Median Rent & Number of Listing' ]
+[x] df = data['Median Rent_YoY (%)']
+[\] df = data['Median Rent by Bedrooms']
+[ ] df = data['Median Rent by Bedrooms YoY(%)' ]
+[ ] df = data['Median Rent by Type']
+[ ] df = data['Median Rent by Type YoY(%)']
+
 """
 
 import os
 import matplotlib.pyplot as plt
 
-from utils import read_data_from_excelfile, median_rent_YoY
+from utils import (read_data_from_excelfile, median_rent_YoY,
+                   common_df_reformatting)
 
 
 folder_path = os.getcwd() + '/data/Rent_Data_March2022_.xlsx'
 data = read_data_from_excelfile(folder_path)
 
+
 city = ['PITTSBURGH, PA', 'ATLANTA, GA'] # all caps
 
 
 #=====================================================
-# Only for final dataframe
+# Working with median rent by bedrooms
+# 
+# 2 functions
+# - input city, return plots for # bedrroms
+# - input cities and # br, return plot with those cities and specification
 #=====================================================
-# df = data['Median Rent_YoY (%)']
-# name_of_sheet = df.iloc[0,0]
 
-# df = df.iloc[1:,:] # for median rent dataframe only, no secondary indexing
+df = data['Median Rent by Bedrooms']
 
-# # renaming column headers based on row below it
-# rename_mapper = dict()
-# for col in df.columns:
-#     rename_mapper[col] = df[col].iloc[0]
-    
-# df.rename(columns=rename_mapper, inplace=True)    
-# df = df.iloc[1:,:] # resetting index once more. 
-
-# df.set_index('Largest 100 Cities', inplace=True)
-# for c in city:    
-#     df.loc[c].plot(label=c)
-    
-# plt.xlabel('date')
-# plt.ylabel('percent (%) change')
-# plt.title(f'{name_of_sheet}')
-# plt.legend()
-
-
-
-test = median_rent_YoY(df=data['Median Rent_YoY (%)'], cities=city, plot=True)
+name_of_sheet = df.iloc[0,0]
+common_df_reformatting(df)
